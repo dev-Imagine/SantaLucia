f53d9a7062e0d8398e991b16ee347d4776892b5a
Nicolas <developer.imagine@gmail.com>
2018-09-10 19:27:05 -0300

    Primer commit

---
+++ SantaLucia/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SantaLucia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login(bool DatosIncorrectos = false)
+        {
+            ViewBag.DatosIncorrectos = DatosIncorrectos;
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Ingresar(string dni, string contraseña)
+        {
+            return RedirectToAction("ListarProducto", "Producto");
+           
+        }
+        public ActionResult CerrarSession()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ SantaLucia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SantaLucia.MvcApplication" Language="C#" %>
